4de783bee2419fcb58ad597bfd48bc0634a15127
Shaun Gill <sgill@wildlifetrusts.org>
2017-01-10 14:58:58 +0000

    up to pt11

---
+++ app/Controllers/Auth/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers\Auth;
+
+use App\Models\User;
+use App\Controllers\Controller;
+
+class AuthController extends Controller
+{
+	public function getSignUp($request, $response)
+	{
+		return $this->view->render($response, 'auth/signup.twig');
+	}	
+
+	public function postSignUp($request, $response)
+	{
+		$user = User::create([
+			'email' => $request->getParam('email'),
+			'name' => $request->getParam('name'),
+			'password' => password_hash($request->getParam('password'), PASSWORD_DEFAULT),
+		]);
+
+		return $response->withRedirect($this->router->pathFor('home'));
+	}
+}
---
+++ bootstrap/app.php
@@ -0,0 +1,55 @@
+<?php
+
+session_start();
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new \Slim\App([
+	'settings' => [
+		'displayErrorDetails' => true,
+		'db' => [
+			'driver' => 'mysql',
+			'host' => 'localhost',
+			'database' => 'auth',
+			'username' => 'root',
+			'password' => 'wildlife7238',
+			'charset' => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix' => '',
+		]
+	],
+]);
+
+$container = $app->getContainer();
+
+$capsule = new \Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($container['settings']['db']);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$container['db'] = function($container) use ($capsule) {
+	return $capsule;
+};
+
+$container['view'] = function($container) {
+	$view = new \Slim\Views\Twig(__DIR__ . '/../resources/views', [
+		'cache' => false,
+	]);
+
+	$view->addExtension(new \Slim\Views\TwigExtension(
+		$container->router,
+		$container->request->getUri()
+	));
+
+	return $view;
+};
+
+$container['HomeController'] = function($container) {
+	return new \App\Controllers\HomeController($container);
+};
+
+$container['AuthController'] = function($container) {
+	return new \App\Controllers\Auth\AuthController($container);
+};
+
+require __DIR__ . '/../app/routes.php';
