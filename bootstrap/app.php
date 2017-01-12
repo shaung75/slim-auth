1b56aad35c65f404f86faa288b969bc6210b1273
Shaun Gill <sgill@wildlifetrusts.org>
2017-01-12 14:41:51 +0000

    Done pt 14

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use Respect\Validation\Validator as v;
+
 session_start();
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -58,5 +60,7 @@ $container['AuthController'] = function($container) {
 
 $app->add(new \App\Middleware\ValidationErrorsMiddleware($container));
 $app->add(new \App\Middleware\OldInputMiddleware($container));
+
+v::with('App\\Validation\\Rules\\');
 
 require __DIR__ . '/../app/routes.php';
