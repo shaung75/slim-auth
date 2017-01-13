2cfc2d9dfb8959f81f7ca2aa031957491c1d2bef
Shaun Gill <sgill@wildlifetrusts.org>
2017-01-13 10:17:52 +0000

    pt28

--- app/Controllers/Auth/PasswordController.php
+++ app/Controllers/Auth/PasswordController.php
@@ -15,6 +15,18 @@ class PasswordController extends Controller
 
 	public function postChangePassword($request, $response)
 	{
+		$validation = $this->validator->validate($request, [
+			'password_old' => v::notEmpty()->matchesPassword($this->auth->user()->password),
+			'password' => v::notEmpty(),
+		]);
 
+		if ($validation->failed()) {
+			return $response->withRedirect($this->router->pathFor('auth.password.change'));
+		}
+
+		$this->auth->user()->setPassword($request->getParam('password'));
+
+		$this->flash->addMessage('info', 'Password has been changed');
+		return $response->withRedirect($this->router->pathFor('home'));
 	}
 }
